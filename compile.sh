6fd471de47bdbe9b807e04b41b749eb72aaa5461
a00908270 <a00908270@unet.univie.ac.at>
2018-04-27 10:13:25 +0200

    overview main services image, compile to pdf

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
 #pandoc abstract.md -o latex/abstract.tex --top-level-division=chapter
 pandoc thesis.md -o latex/thesis.tex --top-level-division=chapter
 sed -i '' 's/.png//g' latex/thesis.tex
+
+cd latex
+pdflatex Container\ Based\ Execution\ Stack\ for\ Neural\ Networks.tex
+bibtex Container\ Based\ Execution\ Stack\ for\ Neural\ Networks
+pdflatex Container\ Based\ Execution\ Stack\ for\ Neural\ Networks.tex
+#pdflatex --shell-escape Container\ Based\ Execution\ Stack\ for\ Neural\ Networks.tex
